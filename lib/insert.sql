ef82d599c52f77254fdc1effb172e44a68df0ec6
Carlos <carlos.alfonso.montero@gmail.com>
2021-07-21 22:48:09 -0400

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age) 
+VALUES ('Mr. Chocolate', 'M'),
+('Rowdy', 'M'),
+('Tabitha', 'F'),
+('Sergeant Brown', 'M'),
+('Melissa', 'F'),
+('Grinch', 'M'),
+('Wendy', 'F'),
+('unnamed', 'M'); 
